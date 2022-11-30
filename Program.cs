72ce2b4a0af64210d2a89cff27a869a2b9bc3d2f
chingchingchingchong <119520221+chingchingchingchong@users.noreply.github.com>
2022-11-30 18:07:52 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMORPG2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfHeroes = int.Parse(Console.ReadLine());
+
+            string[] names = new string[numberOfHeroes];
+            int[] health = new int[numberOfHeroes];
+            int[] mana = new int[numberOfHeroes];
+
+            for (int i = 0; i < numberOfHeroes; i++)
+            {
+                string[] stats = Console.ReadLine().Split().ToArray();
+
+                names[i] = stats[0];
+                health[i] = int.Parse(stats[1]);
+                mana[i] = int.Parse(stats[2]);
+            }
+
+            while(true)
+            {
+                string[] commands = Console.ReadLine().Split().ToArray();
+
+                if (commands[0] == "End")
+                {
+                    break;
+                }
+
+                switch (commands[0])
+                {
+                    case "minHP":
+                        int Index = Array.IndexOf(health, health.Min());
+
+                        Console.WriteLine($"{names[Index]} - HP: {health[Index]}");
+                        break;
+
+                    case "maxMP":
+                        int indexMp = Array.IndexOf(mana, mana.Max());
+                        Console.WriteLine($"{names[indexMp]} - MP: {mana[indexMp]}");
+                        break;
+
+                    case "Display":
+                            Console.WriteLine($"{string.Join(", ", names)}");
+                            Console.WriteLine($"{string.Join(", ", health)}");
+                            Console.WriteLine($"{string.Join(", ", mana)}");
+                        break;
+
+                    case "Reverse":
+                        names = names.Reverse().ToArray();
+                        health = health.Reverse().ToArray();
+                        mana = mana.Reverse().ToArray();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid command");
+                        break;
+                }
+            }
+        }
+    }
+}
